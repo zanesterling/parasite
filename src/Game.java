588f4be8b1465f6cf4a8b450f407e64d94b39144
Zane Sterling <sterling.zane@gmail.com>
2014-11-19 12:52:28 -0500

    Add skeleton Game class

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+package Parasite;
+
+public class Game {
+
+	public Game() {
+		// TODO Implement Game()
+	}
+
+	public void run() {
+		// TODO Implement Game.run()
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.run();
+	}
+}
