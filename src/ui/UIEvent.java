0fa3c5976c936a0c12805223a50d01f88302725e
Zane Sterling <sterling.zane@gmail.com>
2014-11-20 13:08:19 -0500

    Change UIEvent "eventType" to an int "eventCode"
    Also add some sample eventCodes

--- src/ui/UIEvent.java
+++ src/ui/UIEvent.java
@@ -2,27 +2,38 @@ package Parasite.ui;
 
 public class UIEvent {
 
-	public String eventType;
+	// eventCode codes
+	public static final int MOVE_UP = 0;
+	public static final int MOVE_DOWN = 1;
+	public static final int MOVE_LEFT = 2;
+	public static final int MOVE_RIGHT = 3;
+
+	public static final int STOP_UP = 4;
+	public static final int STOP_DOWN = 5;
+	public static final int STOP_LEFT = 6;
+	public static final int STOP_RIGHT = 7;
+
+	public int  eventCode;
 	public int[]    iargs;
 	public double[] dargs;
 
-	public UIEvent(String eventType, int[] iargs, double[] dargs) {
-		this.eventType = eventType;
+	public UIEvent(int eventCode, int[] iargs, double[] dargs) {
+		this.eventCode = eventCode;
 		this.iargs = iargs;
 		this.dargs = dargs;
 	}
 
-	public UIEvent(String eventType, double[] dargs) {
-		this.eventType = eventType;
+	public UIEvent(int eventCode, double[] dargs) {
+		this.eventCode = eventCode;
 		this.dargs = dargs;
 	}
 
-	public UIEvent(String eventType, int[] iargs) {
-		this.eventType = eventType;
+	public UIEvent(int eventCode, int[] iargs) {
+		this.eventCode = eventCode;
 		this.iargs = iargs;
 	}
 
-	public UIEvent(String eventType) {
-		this.eventType = eventType;
+	public UIEvent(int eventCode) {
+		this.eventCode = eventCode;
 	}
 }
